c1f45c6fd7d73bec6392f90b5cc3846427bc3b96
Tikam Singh <tikamsingh1901900@gmail.com>
2025-02-02 13:58:53 +0000

    System info gathering

--- system_automation.sh
+++ system_automation.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Text color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+RESET='\033[0m'  # Reset color to default
+
 echo "hello, world!"
 
 #File and Directory Operations
@@ -8,4 +15,15 @@ touch file1.txt file2.txt
 echo "this is file1" >file1.txt
 mkdir test_dir
 echo "this is a directory" > test_dir/info.txt
+
+#system info 
+echo -e "${RED}Gathering system information.${RESET}"
+echo "CPU info:"
+lscpu
+echo -e "${RED}Memory Usage:${RESET}"
+free -h
+echo -e "${RED}disc usage:${RESET}"
+df -h
+echo -e "${RED}system uptime${RESET}"
+uptime
 
