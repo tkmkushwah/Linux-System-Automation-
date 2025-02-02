f0d85825892600a012b3374a86201c11dcc2f28b
Tikam Singh <tikamsingh1901900@gmail.com>
2025-02-02 13:40:30 +0000

    files and directory ops

---
+++ system_automation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "hello, world!"
+
+#File and Directory Operations
+echo "creating files and directiories"
+touch file1.txt file2.txt
+echo "this is file1" >file1.txt
+mkdir test_dir
+echo "this is a directory" > test_dir/info.txt
+
